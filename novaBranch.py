79462da8c531f4322b8574dcc8b94b50bc56d231
Albert <albrtcd@gmail.com>
2023-09-23 11:56:13 -0300

    add de cod

--- novaBranch.py
+++ novaBranch.py
@@ -1,4 +1,4 @@
 calc = int(input('Digite um numero: '))
 mult = calc*3
 div = calc/3
-print(f'O numero digitado foi {calc} e sua mutiplicao por 3 é {mult} e sua divisão é {div}')
+print(f'O numero digitado foi {calc} e sua mutiplicao por três é {mult} e sua divisão é {div}')
